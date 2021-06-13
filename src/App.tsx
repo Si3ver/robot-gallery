bb17f9620385f4d80f8401d292e7e2a23ad7b117
Si3ver <izhouwl@163.com>
2021-06-13 22:12:14 +0800

    feat: 引入logo和标题

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import robots from "./mock/rotobs.json";
 import Robot from "./components/Robot";
+import logo from "./assets/images/logo.svg";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img className={styles.appLogo} src={logo} alt="logo" />
+        <h1>老铁，来个机器人🤖️不</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map(({ id, name, email }) => (
           <Robot id={id} name={name} email={email} />
